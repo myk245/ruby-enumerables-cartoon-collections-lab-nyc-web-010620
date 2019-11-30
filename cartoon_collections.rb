daff8f05354807110b09c3e7927d98817d910435
Cindy Kei <myk245@nyu.edu>
2019-11-30 23:35:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,5 +26,6 @@ def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   strings.find do |cheese|
-
+    cheese_types.include? (cheese)
+  end
 end
