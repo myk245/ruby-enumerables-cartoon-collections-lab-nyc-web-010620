ab1033c97a13d6a999c8129929caa6868febf043
Cindy Kei <myk245@nyu.edu>
2019-11-30 23:04:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,11 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-
+  calls.each do |word|
+    if word.length > 4
+      return true
+    else return false
+  end 
 end
 
 def find_the_cheese# code an argument here
