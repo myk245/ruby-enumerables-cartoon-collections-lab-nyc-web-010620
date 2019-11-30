50deb03d458c6a55e7f37ee025d2283ce78f4900
Cindy Kei <myk245@nyu.edu>
2019-11-30 23:30:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,16 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(array_of_calls)
-  array_of_calls.any? { |x| x.length > 4 }
+  array_of_calls.any? { |word| word.length > 4 }
 end
+#answer = false
+#  calls.each do |word|
+#    if word.length > 4
+#      answer = true
+#  end
+#end
+#answer
+#end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
